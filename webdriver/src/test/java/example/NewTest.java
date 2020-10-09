e8e1cc68e29519ef71e44dbf4cbae0fa0d35cf1a
Ta Quoc An <tqan04@gmail.com>
2020-10-09 13:43:05 +0700

    4th commit

--- webdriver/src/test/java/example/NewTest.java
+++ webdriver/src/test/java/example/NewTest.java
@@ -1,8 +1,10 @@
 package example;		
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;		
-import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;		
 import org.testng.annotations.Test;	
 import org.testng.annotations.BeforeTest;	
@@ -11,14 +13,19 @@ public class NewTest {
 	    private WebDriver driver;		
 		@Test				
 		public void testEasy() {	
-			driver.get("http://www.google.com/");  
+			driver.get("https://www.google.com/");  
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Google")); 		
 		}	
 		@BeforeTest
 		public void beforeTest() {
 			System.setProperty("webdriver.gecko.driver","C:\\Users\\tqan\\Downloads\\geckodriver-v0.27.0-win64\\geckodriver.exe");
-		    driver = new FirefoxDriver();  
+			Proxy proxy = new Proxy();
+			proxy.setHttpProxy("10.225.3.1:3128");
+			proxy.setSslProxy("10.225.3.1:3128");
+			FirefoxOptions options = new FirefoxOptions();
+			options.setCapability("proxy", proxy);
+		    driver = new FirefoxDriver(options);  
 		}		
 		@AfterTest
 		public void afterTest() {
