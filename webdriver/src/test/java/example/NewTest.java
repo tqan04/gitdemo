1816e12477fe0d53be383674dd5bbb15702b8c7e
Ta Quoc An <tqan04@gmail.com>
2020-10-09 11:16:27 +0700

    1rst commit

---
+++ webdriver/src/test/java/example/NewTest.java
@@ -0,0 +1,27 @@
+package example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://www.google.com");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Google")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\tqan\\Downloads\\geckodriver-v0.27.0-win64\\geckodriver.exe");
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
